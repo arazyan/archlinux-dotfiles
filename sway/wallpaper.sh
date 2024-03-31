fefa85c846e257f7313aea88ef1eb6bfa74733a4
Araz Abdyev <arazdatas@gmail.com>
2024-03-31 20:44:31 +0300

    upd: add feature

--- sway/wallpaper.sh
+++ sway/wallpaper.sh
@@ -1,1 +1,1 @@
-mpvpaper -o "input-ipc-server=/tmp/mpv-socket" DP-9 ~/Downloads/hu-tao-genshin.3840x2160.mp4
+mpvpaper -o "no-audio --loop-playlist input-ipc-server=/tmp/mpv-socket" DP-9 ~/Downloads/hu-tao-genshin.3840x2160.mp4
